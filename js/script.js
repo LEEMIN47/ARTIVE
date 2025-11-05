a319c33e35e63e7be62e49b2f4a06cc2a7260651
LEEMIN47 <leeminlwjltw@gmail.com>
2025-11-05 17:52:31 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-function loadArchive(artistName, containerId) {
+ function loadArchive(artistName, containerId, event) {
   const container = document.getElementById(containerId);
   const button = document.querySelector(`[onclick*="${artistName}"]`);
 
-    const filenameMap = {
+  const filenameMap = {
     '김하린': 'harin',
     '이도현': 'dohyun',
     '아야카모리': 'ayaka',
@@ -46,91 +46,91 @@ function loadArchive(artistName, containerId) {
 
 
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-        var typingBool = false;
-        var typingIdx = 0;
-        var liIndex = 0;
-        var liLength = $(".typing-txt>ul>li").length;
+  var typingBool = false;
+  var typingIdx = 0;
+  var liIndex = 0;
+  var liLength = $(".typing-txt>ul>li").length;
 
-        // 타이핑될 텍스트를 가져온다 
-        var typingTxt = $(".typing-txt>ul>li").eq(liIndex).text();
-        //liIndex 인덱스로 구분해 한줄씩 가져옴
+  // 타이핑될 텍스트를 가져온다 
+  var typingTxt = $(".typing-txt>ul>li").eq(liIndex).text();
+  //liIndex 인덱스로 구분해 한줄씩 가져옴
 
-        typingTxt = typingTxt.split(""); // 한글자씩 잘라 배열로만든다
+  typingTxt = typingTxt.split(""); // 한글자씩 잘라 배열로만든다
 
-        if (typingBool == false) { // 타이핑이 진행되지 않았다면 
-            typingBool = true;
-            var tyInt = setInterval(typing, 100); // 반복동작 
-        }
+  if (typingBool == false) { // 타이핑이 진행되지 않았다면 
+    typingBool = true;
+    var tyInt = setInterval(typing, 100); // 반복동작 
+  }
 
-        function typing() {
-            $(".typing ul li").removeClass("on");
-            $(".typing ul li").eq(liIndex).addClass("on");
-            //현재 타이핑되는 문장에만 커서 애니메이션을 넣어준다.
+  function typing() {
+    $(".typing ul li").removeClass("on");
+    $(".typing ul li").eq(liIndex).addClass("on");
+    //현재 타이핑되는 문장에만 커서 애니메이션을 넣어준다.
 
-            if (typingIdx < typingTxt.length) { // 타이핑될 텍스트 길이만큼 반복 
-                $(".typing ul li").eq(liIndex).append(typingTxt[typingIdx]); // 한글자씩 이어준다. 
-                typingIdx++;
-            } else { //한문장이끝나면
-                if (liIndex < liLength - 1) {
-                    //다음문장으로  가기위해 인덱스를 1증가
-                    liIndex++;
-                    //다음문장을 타이핑하기위한 셋팅
-                    typingIdx = 0;
-                    typingBool = false;
-                    typingTxt = $(".typing-txt>ul>li").eq(liIndex).text();
+    if (typingIdx < typingTxt.length) { // 타이핑될 텍스트 길이만큼 반복 
+      $(".typing ul li").eq(liIndex).append(typingTxt[typingIdx]); // 한글자씩 이어준다. 
+      typingIdx++;
+    } else { //한문장이끝나면
+      if (liIndex < liLength - 1) {
+        //다음문장으로  가기위해 인덱스를 1증가
+        liIndex++;
+        //다음문장을 타이핑하기위한 셋팅
+        typingIdx = 0;
+        typingBool = false;
+        typingTxt = $(".typing-txt>ul>li").eq(liIndex).text();
 
-                    //다음문장 타이핑전 1초 쉰다
-                    clearInterval(tyInt);
-                    //타이핑종료
+        //다음문장 타이핑전 1초 쉰다
+        clearInterval(tyInt);
+        //타이핑종료
 
-                    setTimeout(function () {
-                        //1초후에 다시 타이핑 반복 시작
-                        tyInt = setInterval(typing, 100);
-                    }, 1000);
-                } else if (liIndex == liLength - 1) {
+        setTimeout(function () {
+          //1초후에 다시 타이핑 반복 시작
+          tyInt = setInterval(typing, 100);
+        }, 1000);
+      } else if (liIndex == liLength - 1) {
 
-                    //마지막 문장까지 써지면 반복종료
-                    clearInterval(tyInt);
+        //마지막 문장까지 써지면 반복종료
+        clearInterval(tyInt);
 
-                    //1초후
-                    setTimeout(function () {
-                        //사용했던 변수 초기화
-                        typingBool = false;
-                        liIndex = 0;
-                        typingIdx = -0;
+        //1초후
+        setTimeout(function () {
+          //사용했던 변수 초기화
+          typingBool = false;
+          liIndex = 0;
+          typingIdx = -0;
 
-                        //첫번째 문장으로 셋팅
-                        typingTxt = $(".typing-txt>ul>li").eq(liIndex).text();
-                        //타이핑 결과 모두 지우기
-                        $(".typing ul li").html("")
+          //첫번째 문장으로 셋팅
+          typingTxt = $(".typing-txt>ul>li").eq(liIndex).text();
+          //타이핑 결과 모두 지우기
+          $(".typing ul li").html("")
 
-                        //반복시작
-                        tyInt = setInterval(typing, 100);
-                    }, 1000);
+          //반복시작
+          tyInt = setInterval(typing, 100);
+        }, 1000);
 
 
-                }
-            }
-        }
+      }
+    }
+  }
 });
 
 function closeArchive() {
-    // 모든 archive-container 닫기
-    document.querySelectorAll('.archive-container').forEach(div => {
-        div.style.display = 'none';
-        div.innerHTML = '';
-    });
+  // 모든 archive-container 닫기
+  document.querySelectorAll('.archive-container').forEach(div => {
+    div.style.display = 'none';
+    div.innerHTML = '';
+  });
 
-    // 모든 View Archive 버튼 비활성화
-    document.querySelectorAll('.button').forEach(btn => btn.classList.remove('active'));
+  // 모든 View Archive 버튼 비활성화
+  document.querySelectorAll('.button').forEach(btn => btn.classList.remove('active'));
 
-    // .artists 섹션으로 스크롤 이동
-    const artistsSection = document.querySelector('.artists');
-    if (artistsSection) {
-        artistsSection.scrollIntoView({ behavior: 'smooth' });
-    }
+  // .artists 섹션으로 스크롤 이동
+  const artistsSection = document.querySelector('.artists');
+  if (artistsSection) {
+    artistsSection.scrollIntoView({ behavior: 'smooth' });
+  }
 }
 
 
@@ -167,5 +167,19 @@ images.forEach((img, i) => {
   img.addEventListener('mouseenter', () => {
     current = i;
     updatePositions();
+  });
+});
+
+
+
+document.querySelectorAll('.button').forEach(btn => {
+  btn.addEventListener('mousemove', e => {
+    const trail = document.createElement('div');
+    trail.classList.add('button-trail');
+    trail.style.left = e.pageX + 'px';
+    trail.style.top = e.pageY + 'px';
+    document.body.appendChild(trail);
+
+    setTimeout(() => trail.remove(), 500);
   });
 });
